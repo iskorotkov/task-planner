24d73a7df8dc500852f9c9c567d54dc19ed3b7e0
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2020-05-01 21:03:19 +0500

    Add modal for creation of new tasks

---
+++ Client/Shared/Tasks/CreateTask.razor.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using TaskPlanner.Shared.Data.Tasks;
+
+namespace TaskPlanner.Client.Shared.Tasks
+{
+    public partial class CreateTask
+    {
+        [Parameter] public EventCallback<Todo> TaskCreated { get; set; }
+        // [Parameter] public EventCallback? CreationCanceled { get; set; }
+
+        [Parameter(CaptureUnmatchedValues = true)]
+        public Dictionary<string, object> AdditionalAttributes { get; set; }
+
+        private readonly Todo _task;
+        private readonly EditContext _context;
+
+        public CreateTask()
+        {
+            _task = new Todo();
+            _context = new EditContext(_task);
+        }
+
+        private async Task Submit()
+        {
+            if (_context.Validate())
+            {
+                if (TaskCreated.HasDelegate)
+                {
+                    await TaskCreated.InvokeAsync(_task);
+                }
+            }
+        }
+    }
+}
