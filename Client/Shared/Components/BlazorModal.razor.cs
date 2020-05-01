13e0bb3472dd0cea59a8e11fe69aa474f39991c2
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2020-05-02 01:07:11 +0500

    Add BlazorModal component

---
+++ Client/Shared/Components/BlazorModal.razor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Components;
+
+namespace TaskPlanner.Client.Shared.Components
+{
+    public partial class BlazorModal
+    {
+        [Parameter] public bool UseFade { get; set; } = true;
+        [Parameter] public bool IsShown { get; set; } = false;
+        [Parameter] public bool UseBackdrop { get; set; } = true;
+
+        [Parameter] public RenderFragment ModalHeader { get; set; }
+        [Parameter] public RenderFragment ModalBody { get; set; }
+        [Parameter] public RenderFragment ModalFooter { get; set; }
+
+        [Parameter(CaptureUnmatchedValues = true)]
+        public Dictionary<string, object> AdditionalAttributes { get; set; }
+
+        public void Show() => IsShown = true;
+        public void Hide() => IsShown = false;
+        public void Toggle() => IsShown = !IsShown;
+
+        private string FadeClass => UseFade ? "fade" : null;
+        private string ShowClass => IsShown ? "show" : null;
+        private string DisplayStyle => IsShown ? "display: block;" : "display: none;";
+    }
+}
