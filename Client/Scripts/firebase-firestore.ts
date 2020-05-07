201cb32bbadc9540497f95a81bad1d4266a31585
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2020-05-07 16:14:14 +0500

    Fix async/await in firestore TS file

--- Client/Scripts/firebase-firestore.ts
+++ Client/Scripts/firebase-firestore.ts
@@ -5,39 +5,51 @@ class Firestore {
     this.db = firebase.firestore()
   }
 
-  getDoc (path: string): firebase.firestore.QueryDocumentSnapshot | any {
-    return this.db.doc(path).get().then(query => query).catch(error => {
-      console.log(error)
-      return error
-    })
+  async getDoc (path: string): Promise<firebase.firestore.DocumentData | null> {
+    try {
+      const doc = await this.db.doc(path).get()
+      return doc.data()
+    } catch (e) {
+      console.log(e)
+      return null
+    }
   }
 
-  getCollection (path: string): firebase.firestore.QuerySnapshot | any {
-    return this.db.collection(path).get().then(query => query).catch(error => {
-      console.log(error)
-      return error
-    })
+  async getCollection (path: string): Promise<firebase.firestore.DocumentData[] | null> {
+    try {
+      const collection = await this.db.collection(path).get()
+      const docs = collection.docs.map(x => x.data())
+      console.log(docs)
+      return docs
+    } catch (e) {
+      console.log(e)
+      return null
+    }
   }
 
-  save (path: string, obj: any): string {
-    const doc = this.db.doc(path)
-    doc.set(obj)
-    return doc.id
+  async save (path: string, obj: any): Promise<void> {
+    try {
+      const doc = this.db.doc(path)
+      await doc.set(obj)
+    } catch (e) {
+      console.log(e)
+    }
   }
 
   delete (path: string) {
     const doc = this.db.doc(path)
-    doc.delete()
+    doc.delete().catch(error => console.log(error))
   }
 
   subscribeToCollection (subscriber: DotNet.DotNetObject,
     path: string,
     onSnapshotMethod = 'OnCollectionSnapshot',
     onErrorMethod = 'OnCollectionError'): object {
-    return this.db.collection(path).onSnapshot(snapshot => {
-      subscriber.invokeMethodAsync(onSnapshotMethod, snapshot)
-    }, error => {
-      subscriber.invokeMethodAsync(onErrorMethod, error)
+    return this.db.collection(path).onSnapshot(async snapshot => {
+      const docs = snapshot.docs.map(x => x.data())
+      await subscriber.invokeMethodAsync(onSnapshotMethod, docs)
+    }, async error => {
+      await subscriber.invokeMethodAsync(onErrorMethod, error)
     })
   }
 
@@ -45,10 +57,10 @@ class Firestore {
     path: string,
     onSnapshotMethod = 'OnDocSnapshot',
     onErrorMethod = 'OnDocError'): object {
-    return this.db.doc(path).onSnapshot(snapshot => {
-      subscriber.invokeMethodAsync(onSnapshotMethod, snapshot)
-    }, error => {
-      subscriber.invokeMethodAsync(onErrorMethod, error)
+    return this.db.doc(path).onSnapshot(async snapshot => {
+      await subscriber.invokeMethodAsync(onSnapshotMethod, snapshot.data())
+    }, async error => {
+      await subscriber.invokeMethodAsync(onErrorMethod, error)
     })
   }
 }
