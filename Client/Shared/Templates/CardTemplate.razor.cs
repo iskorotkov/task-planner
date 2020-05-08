802488de9aaa1aa6546b6764633aa7b312f1243c
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2020-05-08 19:04:25 +0500

    Add card templates

---
+++ Client/Shared/Templates/CardTemplate.razor.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace TaskPlanner.Client.Shared.Templates
+{
+    public partial class CardTemplate
+    {
+        // ReSharper disable once UnusedAutoPropertyAccessor.Global
+        [Parameter] public RenderFragment? Header { get; set; }
+
+        // ReSharper disable once UnusedAutoPropertyAccessor.Global
+        [Parameter] public RenderFragment? Body { get; set; }
+
+        // ReSharper disable once UnusedAutoPropertyAccessor.Global
+        [Parameter] public RenderFragment? Footer { get; set; }
+    }
+}
---
+++ Client/Shared/Templates/SectionCardTemplate.razor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.AspNetCore.Components;
+
+namespace TaskPlanner.Client.Shared.Templates
+{
+    public partial class SectionCardTemplate<TValue>
+    {
+        [Parameter] public TValue Value { get; set; }
+        [Parameter] public RenderFragment<TValue> EditFields { get; set; }
+        [Parameter] public RenderFragment<TValue> Header { get; set; }
+        [Parameter] public EventCallback RemoveSection { get; set; }
+    }
+}
