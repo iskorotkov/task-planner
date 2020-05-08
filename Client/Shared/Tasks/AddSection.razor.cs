233f17ce94acb97618a4a8b3cf98e9c507136c06
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2020-05-08 14:35:05 +0500

    Add padding to 'add section' buttons

---
+++ Client/Shared/Tasks/AddSection.razor.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace TaskPlanner.Client.Shared.Tasks
+{
+    public partial class AddSection
+    {
+        // ReSharper disable once UnusedAutoPropertyAccessor.Global
+        [Parameter] public EventCallback OnClick { get; set; }
+        // ReSharper disable once UnusedAutoPropertyAccessor.Global
+        [Parameter] public string? Text { get; set; }
+    }
+}
