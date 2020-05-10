7cc61e4af5eaeb047e7ad47927e978ba00e062b4
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2020-05-10 22:20:51 +0500

    Fix messages about time left before deadline

---
+++ Shared/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using TaskPlanner.Shared.Data.Spans;
+
+namespace TaskPlanner.Shared.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static bool IsOverdue(this DateTime deadline)
+        {
+            return deadline < DateTime.Now;
+        }
+
+        public static string GetTimeLeftMessage(this DateTime deadline, bool shortVersion = true)
+        {
+            if (deadline.IsOverdue())
+            {
+                return "overdue";
+            }
+            else
+            {
+                var timeLeft = deadline - DateTime.Now;
+                var timeSpan = timeLeft.ToTaskTimeSpan();
+                var str = shortVersion ? timeSpan.ToShortString() : timeSpan.ToLongString();
+                return str + " left";
+            }
+        }
+    }
+}
---
+++ Shared/Extensions/TimeSpanExtensions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using TaskPlanner.Shared.Data.Spans;
+
+namespace TaskPlanner.Shared.Extensions
+{
+    public static class TimeSpanExtensions
+    {
+        public static TaskTimeSpan ToTaskTimeSpan(this TimeSpan span)
+        {
+            if (span.TotalSeconds < 60)
+            {
+                return new TaskTimeSpan((int)span.TotalSeconds, TimePeriod.Seconds);
+            }
+
+            if (span.TotalMinutes < 60)
+            {
+                return new TaskTimeSpan((int)span.TotalMinutes, TimePeriod.Minutes);
+            }
+
+            if (span.TotalHours < 24)
+            {
+                return new TaskTimeSpan((int)span.TotalHours, TimePeriod.Hours);
+            }
+
+            if (span.TotalDays < 7)
+            {
+                return new TaskTimeSpan((int)span.TotalDays, TimePeriod.Days);
+            }
+
+            const int daysInMonth = 30;
+            if (span.TotalDays < daysInMonth)
+            {
+                return new TaskTimeSpan((int)span.TotalDays / 7, TimePeriod.Weeks);
+            }
+
+            const int daysInYear = 365;
+            if (span.TotalDays < daysInYear)
+            {
+                return new TaskTimeSpan((int)span.TotalDays / daysInMonth, TimePeriod.Months);
+            }
+
+            return new TaskTimeSpan((int)span.TotalDays / daysInYear, TimePeriod.Years);
+        }
+    }
+}
