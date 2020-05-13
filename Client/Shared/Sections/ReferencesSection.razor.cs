711180798ffea45a7405272213e0502bd56c5a18
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2020-05-13 16:22:59 +0500

    Add 'references' section

---
+++ Client/Shared/Sections/ReferencesSection.razor.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using TaskPlanner.Client.Services.References;
+using TaskPlanner.Shared.Data.References;
+using TaskPlanner.Shared.Data.Tasks;
+
+namespace TaskPlanner.Client.Shared.Sections
+{
+    public partial class ReferencesSection
+    {
+        [Parameter] public Todo Task { get; set; }
+
+        [Inject] public IReferenceManager ReferenceManager { get; set; }
+
+        private List<ReferenceType> _referenceTypes;
+
+        protected override void OnParametersSet()
+        {
+            if (Task == null)
+            {
+                throw new ArgumentNullException(nameof(Task));
+            }
+
+            _referenceTypes = Task.References
+                .Select(r => r.Type)
+                .Distinct()
+                .ToList();
+        }
+
+        private IEnumerable<Reference> GetReferencesOfType(ReferenceType t)
+        {
+            return Task.References.Where(r => r.Type == t);
+        }
+    }
+}
