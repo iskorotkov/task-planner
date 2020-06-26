e5d9a9d8c95202e4af6969b57d25b9100db425f0
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2020-06-27 00:33:17 +0500

    Refactor /overview, /update and /create pages

--- Client/Pages/Overview.razor.cs
+++ Client/Pages/Overview.razor.cs
@@ -11,28 +11,6 @@ namespace TaskPlanner.Client.Pages
     [Authorize]
     public partial class Overview
     {
-#pragma warning disable 8618
-        // ReSharper disable once MemberCanBePrivate.Global
-        // ReSharper disable once UnusedAutoPropertyAccessor.Global
-        [Inject] public ITaskManager TaskManager { get; set; }
 
-        // ReSharper disable once UnusedAutoPropertyAccessor.Global
-        [Inject] public NavigationManager NavigationManager { get; set; }
-#pragma warning restore 8618
-
-#pragma warning disable 8618
-        private List<Todo> _tasks;
-#pragma warning restore 8618
-
-        protected override async Task OnInitializedAsync()
-        {
-            _tasks = await TaskManager.GetAll().ConfigureAwait(false)
-                     ?? throw new ArgumentException("List of tasks is null", nameof(_tasks));
-        }
-
-        private void CreateTask()
-        {
-            NavigationManager.NavigateTo("/tasks/create");
-        }
     }
 }
